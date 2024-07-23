717b83a0955297d28ffd462dacd72556a40c8efe
Hrushikesh33 <hrushikeshpothiraju@gmail.com>
2024-07-23 12:59:39 +0530

    First jsx code

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const header = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React"
-);
+
+const header = <h1 id="heading">Hi Hrushikesh</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);

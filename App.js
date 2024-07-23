602e8c6557b99595c6273d3b482ad26018cad931
Hrushikesh33 <hrushikeshpothiraju@gmail.com>
2024-07-23 15:11:59 +0530

    Component composition

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Inside = () => {
+  return (<h2>
+      I am soul
+  </h2>)
+}
 
-const header = <h1 id="heading">Hi Hrushikesh</h1>;
+
+const Header = () => {
+  return (
+    <div>
+      <h1>I am Hrushikesh</h1>
+      <Inside />
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(<Header/>);
